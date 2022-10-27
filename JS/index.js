3e3e0b3e96f385c3099b2afddecd7cf832e50b00
AXVIII3 <76608488+AXVIII3@users.noreply.github.com>
2022-10-27 14:33:25 +0530

    splash screen

--- JS/index.js
+++ JS/index.js
@@ -23,7 +23,25 @@ window.addEventListener('scroll', function() {
 
 // Loader
 var loader = document.getElementById('loader');
+
+// Splash Screen
+var splash = document.getElementById('splash');
+var intro = document.getElementById('intro');
+var logo = document.getElementById('logo');
+var logoGraphic = document.getElementById('logoGraphic');
+
 window.addEventListener("load", function() {
     loader.style.display = 'none';
-    document.getElementById('body').style.overflowY = 'scroll';
+    splash.style.height = '100vh';
+    intro.style.display = 'flex';
+    logo.style.animationName = 'fadeUp';
+    logoGraphic.style.animationName = 'fadeUp';
+    intro.style.animationName = 'slideUp';
+    setInterval(deactivateSplash, 1500);
 });
+
+function deactivateSplash() {
+    splash.style.height = '0';
+    intro.style.display = 'none';
+    document.getElementById('body').style.overflowY = 'scroll';
+}
