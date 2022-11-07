04ddacac7fbe66b4f4a50bb45344a0326745679b
PBAS <anirban.roychowdhury@pbacademic.com>
2022-11-07 11:35:30 +0530

    to top btn

--- JS/location.js
+++ JS/location.js
@@ -42,4 +42,24 @@ function deactivateSplash() {
     splash.style.height = '0';
     intro.style.display = 'none';
     document.getElementById('body').style.overflowY = 'scroll';
-}   
+}  
+
+// Get the button
+let mybutton = document.getElementById("toTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
